70007490724978ad78b2106a158aa375da9491d4
Pranav-Balusu <vspbalusu@gmail.com>
2025-10-23 11:06:24 +0530

    created floydWarshall std algo
    Removed comments about shortest path and querying.

---
+++ floydWarshallExample.java
@@ -0,0 +1,33 @@
+class Solution {
+    public void floydWarshallExample(int n, int[][] edges) {
+        final int INF = 1_000_000_000;
+
+        // dist[i][j] = shortest distance from i to j
+        int[][] dist = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dist[i], INF);
+            dist[i][i] = 0;
+        }
+
+        // initialize from edges
+        for (int[] e : edges) {
+            int u = e[0], v = e[1], w = e[2];
+            dist[u][v] = Math.min(dist[u][v], w);
+            // if undirected:
+            dist[v][u] = Math.min(dist[v][u], w);
+        }
+
+        // core Floyd–Warshall
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (dist[i][k] < INF && dist[k][j] < INF) {
+                        dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                    }
+                }
+            }
+        }
+
+     
+    }
+}
